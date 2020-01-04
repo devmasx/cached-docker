c6cdb15a704056c4d3f6f663cc8394e333ba81ac
Miguel Savignano <migue.masx@gmail.com>
2020-01-04 14:16:08 +0100

    test build arguments

--- spec/app_spec.cr
+++ spec/app_spec.cr
@@ -17,4 +17,21 @@ describe App do
       "docker push gcr.io/docker-rails-258302/rails-sqlite",
     ])
   end
+
+  it "#commands" do
+    subject = App.new(
+      "gcr.io/docker-rails-258302/rails-sqlite",
+      "v1",
+      "--dockerfile ./Dockerfile.dev"
+    )
+    subject.commands.should eq([
+      "docker pull gcr.io/docker-rails-258302/rails-sqlite",
+      "docker pull gcr.io/docker-rails-258302/rails-sqlite:cache-build",
+      "docker build --dockerfile ./Dockerfile.dev --cache-from=gcr.io/docker-rails-258302/rails-sqlite:cache-build --cache-from=gcr.io/docker-rails-258302/rails-sqlite --target build .",
+      "docker build --dockerfile ./Dockerfile.dev --cache-from=gcr.io/docker-rails-258302/rails-sqlite:cache-build --cache-from=gcr.io/docker-rails-258302/rails-sqlite -t gcr.io/docker-rails-258302/rails-sqlite -t gcr.io/docker-rails-258302/rails-sqlite:v1 .",
+      "docker push gcr.io/docker-rails-258302/rails-sqlite:cache-build",
+      "docker push gcr.io/docker-rails-258302/rails-sqlite:v1",
+      "docker push gcr.io/docker-rails-258302/rails-sqlite",
+    ])
+  end
 end
