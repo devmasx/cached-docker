df3510f13316cf397111aa7b5d4bba71f13c42c5
Miguel Savignano <migue.masx@gmail.com>
2020-01-04 17:29:47 +0100

    default dockerfile path

--- src/app.cr
+++ src/app.cr
@@ -7,8 +7,8 @@ class App
   include CachedStages
   @cache_stages : Array(Hash(String, String))
 
-  def initialize(@image_name = "", @image_tag = "", @build_params = "", @cache_stage_target = "")
-    @docker_file_path = "./Dockerfile"
+  def initialize(@image_name = "", @image_tag = "", @build_params = "", @cache_stage_target = "", @docker_file_path = "")
+    @docker_file_path = "./Dockerfile" if @docker_file_path == ""
     @image_tag = Time.utc.to_unix.to_s if @image_tag == ""
     @cache_stages = cache_stages
   end
