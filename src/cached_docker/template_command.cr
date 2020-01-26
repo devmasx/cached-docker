3dccca7303896a04ec4a76f5d32184b572c35449
miguemasx <migue.masx@gmail.com>
2020-01-26 17:55:58 +0100

    remove comments

--- src/cached_docker/template_command.cr
+++ src/cached_docker/template_command.cr
@@ -33,18 +33,3 @@ module CachedDocker::TemplateCommand
     end
   end
 end
-
-# execute_command("docker pull #{IMAGE_NAME}")
-
-# system("docker pull #{CACHE_IMAGE_NAME}")
-# puts "continue"
-
-# commands =
-#   "docker build #{BUILDPARAMS} --target #{CACHE_STAGE_TARGET} --cache-from=#{CACHE_IMAGE_NAME} -t #{CACHE_IMAGE_NAME} ." \
-#   " && " \
-#   "docker build #{BUILDPARAMS} --cache-from=#{CACHE_IMAGE_NAME} -t #{IMAGE_NAME} -t #{IMAGE_NAME}:#{IMAGE_TAG} ." \
-#   " && " \
-#   "docker push #{IMAGE_NAME}:#{IMAGE_TAG}"
-
-# # puts commands
-# system(commands)
